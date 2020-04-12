b97b1d8097a358fa16bb9d843b7327b5b126fb24
jpb06 <jpb.06@outlook.com>
2020-04-12 20:51:28 +0200

    Updating mock api server middlewares

--- dev-mock-api/src/dev.api.server.ts
+++ dev-mock-api/src/dev.api.server.ts
@@ -1,5 +1,10 @@
 import * as express from "express";
-import { Express, Response } from "express-serve-static-core";
+import {
+  Express,
+  Request,
+  Response,
+  NextFunction,
+} from "express-serve-static-core";
 import * as bodyParser from "body-parser"; // pull information from HTML POST (express4)
 import * as cors from "cors";
 
@@ -9,6 +14,19 @@ import mapAcceptTeamInvite from "./routes/user/accept.invite.route.js";
 import mapDeclineTeamInvite from "./routes/user/decline.invite.route.js";
 import mapGetUserTimeline from "./routes/user/get.timeline.route.js";
 import mapGetUserTeams from "./routes/user/get.user.teams.route.js";
+import mapGetTeamMembers from "./routes/team/get.team.members.js";
+import mapGetDaily from "./routes/daily/get.daily.js";
+import mapSetDailyDuration from "./routes/daily/set.daily.duration.js";
+import mapRemoveUnforeseenTicket from "./routes/daily/remove.unforeseen.ticket.js";
+import mapAddUnforeseenTicket from "./routes/daily/add.unforeseen.ticket.js";
+import mapAddDoneTicket from "./routes/daily/add.done.ticket.js";
+import mapRemoveDoneTicket from "./routes/daily/remove.done.ticket.js";
+import mapAddFeeling from "./routes/daily/add.feeling.js";
+import mapRemoveFeeling from "./routes/daily/remove.feeling.js";
+import mapAddSubject from "./routes/daily/add.subject.js";
+import mapRemoveSubject from "./routes/daily/remove.subject.js";
+import ErrorHandler from "./middleware/errors.handler.js";
+import NoRouteErrorHandler from "./middleware/no.route.error.handler.js";
 
 let app: Express = express();
 app.use(cors());
@@ -24,6 +42,21 @@ mapAcceptTeamInvite(app);
 mapDeclineTeamInvite(app);
 mapGetUserTimeline(app);
 mapGetUserTeams(app);
+mapGetTeamMembers(app);
+
+mapGetDaily(app);
+mapSetDailyDuration(app);
+mapAddUnforeseenTicket(app);
+mapRemoveUnforeseenTicket(app);
+mapAddDoneTicket(app);
+mapRemoveDoneTicket(app);
+mapAddFeeling(app);
+mapRemoveFeeling(app);
+mapAddSubject(app);
+mapRemoveSubject(app);
+
+app.use(ErrorHandler);
+app.use(NoRouteErrorHandler);
 
 app.set("port", 3002);
 

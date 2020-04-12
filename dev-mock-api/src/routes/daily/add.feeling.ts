94334e574d63dc8cdefea9883d03f2b439fd5283
jpb06 <jpb.06@outlook.com>
2020-04-12 20:49:30 +0200

    Daily routes : handling feelings

---
+++ dev-mock-api/src/routes/daily/add.feeling.ts
@@ -0,0 +1,48 @@
+import { Application } from "express";
+import { Request, Response } from "express-serve-static-core";
+import isAuthenticated from "../../middleware/is.authenticated";
+import { body } from "express-validator";
+import { getUsers } from "../../dbase/fetch.mock.db";
+import { PersistedUser } from "../../types/persisted.user.type";
+import { getOrCreateDaily } from "../../util/daily";
+import { userToTerseUser } from "../../util/types.conversion.helpers";
+import { updateDaily } from "../../dbase/update.mock.db";
+import { mongoObjectId } from "../../util/objectid";
+
+const mapAddFeeling = (server: Application) => {
+  server.post(
+    "/api/daily/feelings/add",
+    isAuthenticated,
+    [
+      body("teamId").isMongoId(),
+      body("date").isString().toDate(),
+      body("type").isInt(),
+      body("comment").isString(),
+    ],
+    (req: Request, res: Response) => {
+      let users = getUsers();
+
+      const creator = users.find((el) => el.email === res.locals.email) as
+        | PersistedUser
+        | undefined;
+
+      if (!creator) {
+        return res.answer(500, "Unable to retrieve feeling creator");
+      }
+
+      const daily = getOrCreateDaily(req.body.teamId, req.body.date);
+      const feeling = {
+        id: mongoObjectId(),
+        type: req.body.type,
+        comment: req.body.comment,
+        creator: userToTerseUser(creator),
+      };
+      daily.feelings.push(feeling);
+      updateDaily(daily);
+
+      return res.populate(feeling);
+    }
+  );
+};
+
+export default mapAddFeeling;
---
+++ dev-mock-api/src/routes/daily/remove.feeling.ts
@@ -0,0 +1,27 @@
+import { Application } from "express";
+import { Request, Response } from "express-serve-static-core";
+import isAuthenticated from "../../middleware/is.authenticated";
+import { body } from "express-validator";
+import { getOrCreateDaily } from "../../util/daily";
+import { updateDaily } from "../../dbase/update.mock.db";
+
+const mapRemoveFeeling = (server: Application) => {
+  server.post(
+    "/api/daily/feelings/remove",
+    isAuthenticated,
+    [
+      body("teamId").isMongoId(),
+      body("date").isString().toDate(),
+      body("id").isMongoId(),
+    ],
+    (req: Request, res: Response) => {
+      const daily = getOrCreateDaily(req.body.teamId, req.body.date);
+      daily.feelings = daily.feelings.filter((el) => el.id !== req.body.id);
+      updateDaily(daily);
+
+      return res.answer(200, "Feeling deleted");
+    }
+  );
+};
+
+export default mapRemoveFeeling;

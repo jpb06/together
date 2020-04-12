c99152ef04c70a7321de6a52d8e874dc30b7662c
jpb06 <jpb.06@outlook.com>
2020-04-12 20:47:54 +0200

    Adding team members route

---
+++ dev-mock-api/src/routes/team/get.team.members.ts
@@ -0,0 +1,25 @@
+import { Application } from "express";
+import { Request, Response } from "express-serve-static-core";
+import isAuthenticated from "../../middleware/is.authenticated";
+import { body } from "express-validator";
+import { getUsers, getTeams } from "../../dbase/fetch.mock.db";
+
+const mapGetTeamMembers = (server: Application) => {
+  server.post(
+    "/api/team/members",
+    isAuthenticated,
+    [body("teamId").isMongoId()],
+    (req: Request, res: Response) => {
+      const teams = getTeams();
+
+      const team = teams.find((el) => el.id === req.body.teamId);
+      if (!team) {
+        return res.answer(404, "Team not found");
+      }
+
+      return res.populate(team.members);
+    }
+  );
+};
+
+export default mapGetTeamMembers;

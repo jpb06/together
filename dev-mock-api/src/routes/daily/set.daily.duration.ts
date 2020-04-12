8f8449b3da22b190f7b34117eee73a8d022f3cb2
jpb06 <jpb.06@outlook.com>
2020-04-12 20:48:28 +0200

    daily route : set duration

---
+++ dev-mock-api/src/routes/daily/set.daily.duration.ts
@@ -0,0 +1,28 @@
+import { Application } from "express";
+import { Request, Response } from "express-serve-static-core";
+import isAuthenticated from "../../middleware/is.authenticated";
+import { body } from "express-validator";
+import { getOrCreateDaily } from "../../util/daily";
+import { updateDaily } from "../../dbase/update.mock.db";
+
+const mapSetDailyDuration = (server: Application) => {
+  server.post(
+    "/api/daily/setDuration",
+    isAuthenticated,
+    [
+      body("teamId").isMongoId(),
+      body("date").isString().toDate(),
+      body("duration").isString().isIn(["0-15", "15-20", "20-30", "20+"]),
+    ],
+    (req: Request, res: Response) => {
+      let daily = getOrCreateDaily(req.body.teamId, req.body.date);
+      daily.durationIndicator = req.body.duration;
+
+      updateDaily(daily);
+
+      return res.answer(200, `Duration set for ${req.body.date}`);
+    }
+  );
+};
+
+export default mapSetDailyDuration;

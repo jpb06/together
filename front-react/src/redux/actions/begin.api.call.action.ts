930fea2b626c8020ce7725b76f6351ddac73ae3d
jpb06 <jpb.06@outlook.com>
2020-04-13 11:03:35 +0200

    Generic actions

--- front-react/src/redux/actions/begin.api.call.action.ts
+++ front-react/src/redux/actions/begin.api.call.action.ts
@@ -1,7 +1,30 @@
-import { notice } from "./util/generic.actions";
-import { BEGIN_API_CALL } from "./util/action.types";
+import { notice, action } from "./util/generic.actions";
+import { BEGIN_API_CALL, BEGIN_API_CALL_DAILY } from "./util/action.types";
 import { Action } from "redux";
+
+export enum DailyFeedbackType {
+  Unknown,
+  Duration,
+  AddUnforeseenTicket,
+  RemoveUnforeseenTicket,
+  AddDoneTicket,
+  RemoveDoneTicket,
+  AddSubject,
+  RemoveSubject,
+  AddFeeling,
+  RemoveFeeling,
+}
 
 const beginApiCallAction = (): Action => notice(BEGIN_API_CALL);
 
+export interface DailyAlterationBeginPayload {
+  type: DailyFeedbackType;
+  term?: string;
+}
+
+const beginDailyApiCallAction = (
+  payload: DailyAlterationBeginPayload
+): Action => action(BEGIN_API_CALL_DAILY, payload);
+
 export default beginApiCallAction;
+export { beginDailyApiCallAction };

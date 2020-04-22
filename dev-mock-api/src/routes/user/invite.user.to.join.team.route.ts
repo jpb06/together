f07281eb633c31be92f655b3becf05f01b4e216c
jpb06 <jpb.06@outlook.com>
2020-04-22 18:33:08 +0200

    Adding routes to invite a user and request to join a team

---
+++ dev-mock-api/src/routes/user/invite.user.to.join.team.route.ts
@@ -0,0 +1,70 @@
+import { Application } from "express";
+import { Request, Response } from "express-serve-static-core";
+import isAuthenticated from "../../middleware/is.authenticated";
+import { body } from "express-validator";
+import getTeamFromInvite from "../../middleware/get.team.from.invite";
+import { persistUser, persistTeam } from "../../dbase/update.mock.db";
+import {
+  teamToBareTeam,
+  userToTerseUser,
+} from "../../util/types.conversion.helpers";
+import * as moment from "moment";
+import { getUsers, getTeams } from "../../dbase/fetch.mock.db";
+import { mongoObjectId } from "../../util/objectid";
+
+const mapInviteUserToJoinTeam = (server: Application) => {
+  server.post(
+    "/api/user/invite",
+    isAuthenticated,
+    [body("teamId").isMongoId(), body("email").isEmail()],
+    (req: Request, res: Response) => {
+      let users = getUsers();
+      let teams = getTeams();
+
+      const referrer = users.find((el) => el.email === res.locals.email);
+      if (!referrer)
+        return res.answer(520, "Unable to get the invite referrer user");
+
+      const invitee = users.find((el) => el.email === req.body.email);
+      if (!invitee)
+        return res.answer(
+          520,
+          "We could not find any user matching this email. Mind checking again the address for any typo?"
+        );
+
+      const team = teams.find((el) => el.id === req.body.teamId);
+      if (!team) return res.answer(520, "Unable to locate the selected team");
+
+      const isInviteeAlreadyInTeam =
+        team.invitedUsers.find((el) => el.invitee.id === invitee.id) ||
+        team.members.find((el) => el.id === invitee.id);
+      if (isInviteeAlreadyInTeam)
+        return res.answer(520, "This user has already been added to the team");
+
+      const requestId = mongoObjectId();
+      const requestDate = moment().toDate();
+      const terseReferrer = userToTerseUser(referrer);
+      const terseInvitee = userToTerseUser(invitee);
+
+      team.invitedUsers.push({
+        id: requestId,
+        date: requestDate.toISOString(),
+        referrer: terseReferrer,
+        invitee: terseInvitee,
+      });
+      invitee.teamInvites.push({
+        id: requestId,
+        date: requestDate.toISOString(),
+        referrer: terseReferrer,
+        team: teamToBareTeam(team),
+      });
+
+      persistUser(invitee);
+      persistTeam(team);
+
+      return res.populate(terseInvitee);
+    }
+  );
+};
+
+export default mapInviteUserToJoinTeam;
---
+++ dev-mock-api/src/routes/user/request.to.join.team.route.ts
@@ -0,0 +1,60 @@
+import { Application } from "express";
+import { Request, Response } from "express-serve-static-core";
+import isAuthenticated from "../../middleware/is.authenticated";
+import { body } from "express-validator";
+import getTeamFromInvite from "../../middleware/get.team.from.invite";
+import { persistUser, persistTeam } from "../../dbase/update.mock.db";
+import {
+  teamToBareTeam,
+  userToTerseUser,
+} from "../../util/types.conversion.helpers";
+import * as moment from "moment";
+import { getUsers, getTeams } from "../../dbase/fetch.mock.db";
+import { mongoObjectId } from "../../util/objectid";
+
+const mapRequestToJoinTeam = (server: Application) => {
+  server.post(
+    "/api/user/requestToJoinTeam",
+    isAuthenticated,
+    [body("teamName").isString().notEmpty()],
+    (req: Request, res: Response) => {
+      let users = getUsers();
+      let teams = getTeams();
+
+      const user = users.find((el) => el.email === res.locals.email);
+      if (!user) return res.answer(520, "Unable to get the source user");
+
+      const team = teams.find((el) => el.id === req.body.teamId);
+      if (!team) return res.answer(520, "Unable to locate the selected team");
+
+      const userIsAlreadyInTeam =
+        team.joinRequests.find((el) => el.user.id === user.id) ||
+        team.members.find((el) => el.id === user.id);
+      if (userIsAlreadyInTeam)
+        return res.answer(
+          520,
+          "This user has already either been added to the team or requested to join the team"
+        );
+
+      const requestId = mongoObjectId();
+      const requestDate = moment().toISOString();
+      user.teamJoinRequests.push({
+        id: requestId,
+        date: requestDate,
+        team: teamToBareTeam(team),
+      });
+      team.joinRequests.push({
+        id: requestId,
+        date: requestDate,
+        user: userToTerseUser(user),
+      });
+
+      persistUser(user);
+      persistTeam(team);
+
+      return res.answer(200, "Join request sent");
+    }
+  );
+};
+
+export default mapRequestToJoinTeam;

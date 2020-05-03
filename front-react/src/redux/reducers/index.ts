f2d7e724ed7b2c5c4f65e149483645f09ef3a629
jpb06 <jpb.06@outlook.com>
2020-05-03 11:46:00 +0200

    Redux: amending root reducer

--- front-react/src/redux/reducers/index.ts
+++ front-react/src/redux/reducers/index.ts
@@ -1,35 +1,37 @@
 import { combineReducers } from "redux";
-import { userReducer } from "./user.reducer";
-import { userTeamsReducer } from "./user.teams.reducer";
-import { snackbarFeedbackReducer } from "./global/snackbar.feedback.reducer";
-import { apiStatusReducer } from "./global/api.status.reducer";
-import { errorReducer } from "./global/error.reducer";
-import { timelineReducer } from "./timeline.reducer";
-import { dailyReducer } from "./daily.reducer";
-import { teamMembersReducer } from "./team.members.reducer";
-import { dailyDurationFeedbackReducer } from "./daily-feedback/daily.duration.feedback.reducer";
-import { dailyUnforeseenFeedbackReducer } from "./daily-feedback/daily.unforeseen.feedback.reducer";
-import { dailyDoneFeedbackReducer } from "./daily-feedback/daily.done.feedback.reducer";
-import { dailySubjectsFeedbackReducer } from "./daily-feedback/daily.subjects.reducer";
-import { dailyFeelingsFeedbackReducer } from "./daily-feedback/daily.feelings.reducer";
+import userReducer from "./user/user.reducer";
+import userTeamsReducer from "./user/user.teams.reducer";
+import snackbarFeedbackReducer from "./global/snackbar.feedback.reducer";
+import apiStatusReducer from "./global/api.status.reducer";
+import errorReducer from "./global/error.reducer";
+import timelineReducer from "./user/timeline.reducer";
+import dailyReducer from "./user/daily.reducer";
+import teamMembersReducer from "./user/team.members.reducer";
+import dailyDurationFeedbackReducer from "./daily-feedback/daily.duration.feedback.reducer";
+import dailyUnforeseenFeedbackReducer from "./daily-feedback/daily.unforeseen.feedback.reducer";
+import dailyDoneFeedbackReducer from "./daily-feedback/daily.done.feedback.reducer";
+import dailySubjectsFeedbackReducer from "./daily-feedback/daily.subjects.reducer";
+import dailyFeelingsFeedbackReducer from "./daily-feedback/daily.feelings.reducer";
+import accountCreationStateReducer from "./account-creation/account.creation.state.reducer";
 
 const rootReducer = combineReducers({
-  apiCallsInProgress: apiStatusReducer,
-
   user: userReducer,
   userTeams: userTeamsReducer,
   teamMembers: teamMembersReducer,
   timeline: timelineReducer,
+
   daily: dailyReducer,
-
-  error: errorReducer,
-
-  snackbarFeedback: snackbarFeedbackReducer,
   dailyDurationFeedback: dailyDurationFeedbackReducer,
   dailyUnforeseenTicketsFeedback: dailyUnforeseenFeedbackReducer,
   dailyDoneTicketsFeedback: dailyDoneFeedbackReducer,
   dailySubjectsFeedback: dailySubjectsFeedbackReducer,
   dailyFeelingsFeedback: dailyFeelingsFeedbackReducer,
+
+  accountCreationState: accountCreationStateReducer,
+
+  error: errorReducer,
+  snackbarFeedback: snackbarFeedbackReducer,
+  apiCallsInProgress: apiStatusReducer,
 });
 
 export default rootReducer;

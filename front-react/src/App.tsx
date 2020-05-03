11c124d1c32273420b4fc8176cba56e0409cb132
jpb06 <jpb.06@outlook.com>
2020-05-03 12:02:00 +0200

    Amending routes

--- front-react/src/App.tsx
+++ front-react/src/App.tsx
@@ -3,12 +3,13 @@ import { MuiThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { BrowserRouter, Route } from "react-router-dom";
 import theme from "./create.theme";
-import LoginContainer from "./components/login/LoginContainer";
+import LoginContainer from "./components/anonymous/login/LoginContainer";
 import LoggedInRootContainer from "./components/logged-in/LoggedInRootContainer";
-import FeedbackSnackbar from "./components/feedback/FeedbackSnackbar";
+import FeedbackSnackbar from "./components/generic/feedback/FeedbackSnackbar";
 import TimeLineContainer from "./components/logged-in/timeline/TimeLineContainer";
 import UserAccountContainer from "./components/logged-in/user-account/UserAccountContainer";
 import DailyContainer from "./components/logged-in/daily/DailyContainer";
+import NewAccountContainer from "./components/anonymous/new-user/NewAccountContainer";
 
 const App = () => {
   return (
@@ -16,21 +17,22 @@ const App = () => {
       <CssBaseline />
       <BrowserRouter>
         <Route exact path="/" component={LoginContainer} />
+        <Route exact path="/newaccount" component={NewAccountContainer} />
         <Route
           path="/main"
-          render={props => (
+          render={(props) => (
             <LoggedInRootContainer {...props} Component={TimeLineContainer} />
           )}
         />
         <Route
           path="/daily"
-          render={props => (
+          render={(props) => (
             <LoggedInRootContainer {...props} Component={DailyContainer} />
           )}
         />
         <Route
           path="/account"
-          render={props => (
+          render={(props) => (
             <LoggedInRootContainer
               {...props}
               Component={UserAccountContainer}

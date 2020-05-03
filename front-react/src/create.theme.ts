48834bd8d72d9612d5a706f9cad8012ee79608b4
jpb06 <jpb.06@outlook.com>
2020-05-03 12:02:19 +0200

    Toying with the app background

--- front-react/src/create.theme.ts
+++ front-react/src/create.theme.ts
@@ -4,12 +4,13 @@ const theme = createMuiTheme({
   palette: {
     type: "dark",
     primary: {
-      main: "#49a39f"
+      main: "#49a39f",
     },
     secondary: {
-      main: "#0d3c59"
-    }
-  }
+      main: "#0d3c59",
+    },
+    background: { paper: "darkslategray" },
+  },
 });
 
 export default theme;

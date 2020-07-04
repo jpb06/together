bc30012965eab9121d14761909fb1e8811c782ba
jpb06 <jpb.06@outlook.com>
2020-07-04 07:10:42 +0200

    Updating imports to use the splitted mock data

--- dev-mock-api/src/dbase/create.mock.db.ts
+++ dev-mock-api/src/dbase/create.mock.db.ts
@@ -1,11 +1,13 @@
 import * as fs from "fs";
 import * as path from "path";
 
-import { users, teams, dailies } from "./data/mock.data";
+import { users } from "./data/users.mock.data";
+import { teams } from "./data/teams.mock.data";
+import { dailies } from "./data/dailies.mock.data";
 
 const data = JSON.stringify({ users, teams, dailies });
 const filepath = path.join(__dirname, "data", "db.json");
 
-fs.writeFile(filepath, data, err => {
+fs.writeFile(filepath, data, (err) => {
   err ? console.log(err) : console.log("Mock DB created.");
 });

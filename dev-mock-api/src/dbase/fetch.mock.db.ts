53651ee12124ef72d5050fbb3cce61f1bdb796e9
jpb06 <jp.bois.06@outlook.fr>
2020-07-26 13:59:02 +0200

    Returning proper types for get functions

--- dev-mock-api/src/dbase/fetch.mock.db.ts
+++ dev-mock-api/src/dbase/fetch.mock.db.ts
@@ -1,8 +1,9 @@
 import * as db from "./data/db.json";
 import Database from "../types/database.type";
+import { Team } from "../../../shared/types/interfaces/team.interfaces";
+import { PersistedUser as User } from "../../../shared/types/interfaces/user.interfaces";
+import Daily from "../../../shared/types/interfaces/daily.interfaces";
 
-const getUsers = () => (<Database>db).users;
-const getTeams = () => (<Database>db).teams;
-const getDailies = () => (<Database>db).dailies;
-
-export { getUsers, getTeams, getDailies };
+export const getUsers = () => (<Database>db).users as Array<User>;
+export const getTeams = () => (<Database>db).teams as Array<Team>;
+export const getDailies = () => (<Database>db).dailies as Array<Daily>;

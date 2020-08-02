ae61803905f73b2da8c53aa4da79c839baec8d3d
jpb06 <jp.bois.06@outlook.fr>
2020-08-02 16:02:25 +0200

    Adding a barrel for sagas

---
+++ front-react/src/redux/sagas/index.ts
@@ -0,0 +1,24 @@
+export * from "./app/daily.done.tickets.status.saga";
+export * from "./app/global.status.saga";
+export * from "./app/onboarding.status.saga";
+export * from "./app/snackbar.saga";
+
+export * from "./daily/add.done.ticket.saga";
+export * from "./daily/add.feeling.saga";
+export * from "./daily/add.subject.saga";
+export * from "./daily/add.unforeseen.ticket.saga";
+export * from "./daily/get.daily.saga";
+export * from "./daily/remove.details.saga";
+export * from "./daily/remove.ticket.saga";
+export * from "./daily/set.daily.duration.saga";
+
+export * from "./team/create.team.saga";
+export * from "./team/get.team.members.saga";
+export * from "./team/invite.user.to.team.saga";
+export * from "./team/request.to.join.team.saga";
+
+export * from "./user/answer.team.invite.saga";
+export * from "./user/create.user.saga";
+export * from "./user/get.timeline.saga";
+export * from "./user/get.user.teams.saga";
+export * from "./user/login.saga";

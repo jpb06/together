cc2e46738b43f29ce64fb2d510337997a6000deb
jpb06 <jp.bois.06@outlook.fr>
2020-08-02 16:08:52 +0200

    Getting rid of the maleable redux action type former logic

--- front-react/src/redux/logic/action-types/redux.action.type.generation.ts
+++
@@ -1,7 +0,0 @@
-import { Result, Type, Context } from "../../types/action.types";
-
-export const typeFor = (action: Type, context: Context, result: Result) =>
-  action + result + context;
-
-export const beginApiCallFor = (context: Context) =>
-  Type.beginApiCall + context;

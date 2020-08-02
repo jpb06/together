f58d7cd83d89bb298a7115f55a6df83c1f50bf65
jpb06 <jp.bois.06@outlook.fr>
2020-08-02 15:39:22 +0200

    Relocating redux types to the root types folder

--- front-react/src/redux/types/action.result.ts
+++
@@ -1,5 +0,0 @@
-import ApplicationError from "./application.error.type";
-
-export interface ActionResult extends ApplicationError {
-  success: boolean;
-}
--- front-react/src/redux/types/snackbar.feedback.type.ts
+++
@@ -1,6 +0,0 @@
-import { MessageType } from "../../components/generic/feedback/FeedbackSnackbarContent";
-
-export default interface SnackbarFeedback {
-  type: MessageType;
-  message: string;
-}

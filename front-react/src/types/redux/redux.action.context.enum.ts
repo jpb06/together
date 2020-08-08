7808f43ca3b79ce700d1f38bd73580dcc99e2a46
jpb06 <jp.bois.06@outlook.fr>
2020-08-08 17:15:27 +0200

    Extending contexts to handle daily micro UI feedbacks

--- front-react/src/types/redux/redux.action.context.enum.ts
+++ front-react/src/types/redux/redux.action.context.enum.ts
@@ -2,5 +2,14 @@ export enum ReduxActionContext {
   Global = "GLOBAL",
   Modal = "MODAL",
   Onboarding = "ONBOARDING",
-  Daily = "DAILY",
+  // Daily
+  AddingDoneTicket = "DAILY-ADD-DONE-TICKET",
+  RemovingDoneTicket = "DAILY-DEL-DONE-TICKET",
+  AddingUnforeseenTicket = "DAILY-ADD-UNFORESEEN-TICKET",
+  RemovingUnforeseenTicket = "DAILY-DEL-UNFORESEEN-TICKET",
+  AddingSubject = "DAILY-ADD-SUBJECT",
+  RemovingSubject = "DAILY-DEL-SUBJECT",
+  AddingFeeling = "DAILY-ADD-FEELING",
+  RemovingFeeling = "DAILY-DEL-FEELING",
+  SetDuration = "DAILY-SET-DURATION",
 }

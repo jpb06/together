4256e35183e717eab7d7b64cc49d6390cc7ff6be
jpb06 <jp.bois.06@outlook.fr>
2020-08-08 17:21:36 +0200

    No longer required

--- front-react/src/redux/actions/app/application.status.action.ts
+++
@@ -1,6 +0,0 @@
-import { ApplicationStatus, ReduxActionType } from "../../../types/redux";
-
-export const appStateAction = (status: ApplicationStatus) => ({
-  type: ReduxActionType.ApplicationStatus,
-  payload: status,
-});

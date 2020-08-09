4be661442c8edd7f07f54a7bc10f859fb9919350
jpb06 <jp.bois.06@outlook.fr>
2020-08-09 17:36:29 +0200

    Removing Daily related payload actions

--- front-react/src/types/redux/action.with.payload.interface.ts
+++ front-react/src/types/redux/action.with.payload.interface.ts
@@ -1,12 +1,7 @@
 import { Action } from "redux";
+
 import { ReduxActionType } from "./redux.action.types";
-import { DailyFeedbackType } from "./daily.alteration.payload.interface";
 
 export interface ActionWithPayload<TPayload> extends Action<ReduxActionType> {
   payload: TPayload;
-}
-
-export interface DailyIsolatedPayload<TData> extends Action<DailyFeedbackType> {
-  error?: any;
-  data?: TData;
 }

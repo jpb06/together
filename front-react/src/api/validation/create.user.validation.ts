8ba0632d77ade40a8189316209874ebd35888e77
jpb06 <jp.bois.06@outlook.fr>
2020-08-10 16:40:37 +0200

    No longer required

--- front-react/src/api/validation/create.user.validation.ts
+++
@@ -1,9 +0,0 @@
-import { CreateUserResult } from "../../redux/tasks/user/create.user.task";
-import { ApiResponse } from "../../types/api/api.response.interface";
-
-export const isCreateUserResultValid = (
-  result: ApiResponse<CreateUserResult>
-) => result.payload;
-
-export const isEmailAlreadyInUse = (error: any) =>
-  error.response && error.response.status && error.response.status === 401;

137e5d72c82772047e4a40dd28579b1c84848a7a
jpb06 <jp.bois.06@outlook.fr>
2020-08-10 18:07:08 +0200

    Open modal action shouldn't be a saga

--- front-react/src/redux/actions/workflows/answer.team.invite.modal.actions.ts
+++ front-react/src/redux/actions/workflows/answer.team.invite.modal.actions.ts
@@ -1,7 +1,5 @@
 import { ReduxActionContext as Context, ReduxActionType as Type } from "../../../types/redux";
-import { sagaPayloadAction } from "../generic/payload.action";
+import { payloadAction, sagaPayloadAction } from "../generic/payload.action";
 
-export const showAnswerTeamInviteModalAction = (
-  isOpen: boolean,
-  context: Context = Context.Global
-) => sagaPayloadAction(Type.ShowAnswerTeamInviteModal, context, isOpen);
+export const showAnswerTeamInviteModalAction = (isOpen: boolean) =>
+  payloadAction(Type.ShowAnswerTeamInviteModal, isOpen);

6a8823bb00fb51ca1b357c11a67426629e4d4db8
jpb06 <jp.bois.06@outlook.fr>
2020-08-12 19:32:21 +0200

    Renaming login task

--- front-react/src/redux/sagas/user/login.saga.ts
+++ front-react/src/redux/sagas/user/login.saga.ts
@@ -1,5 +1,5 @@
 import { ReduxActionType as Type } from "../../../types/redux";
-import { loginTask } from "../../tasks/user/login.api.task";
+import { loginTask } from "../../tasks/user/login.task";
 import { safeTakeLeadingFor } from "../generic/safe.take.leading.helper";
 
 export function* watchLogin() {

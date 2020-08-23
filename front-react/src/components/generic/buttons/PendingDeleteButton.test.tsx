9a34600dcc769f9de9f8f6da47d15d8711bcb6a8
jpb06 <jpb.06@outlook.com>
2020-08-23 14:08:25 +0200

    Removing useless imports

--- front-react/src/components/generic/buttons/PendingDeleteButton.test.tsx
+++ front-react/src/components/generic/buttons/PendingDeleteButton.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { logRoles, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import PendingDeleteButton from "./PendingDeleteButton";
 

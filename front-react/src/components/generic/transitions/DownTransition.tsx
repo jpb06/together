47a6cc03f92e2175bc806e88065b8075862a1d70
jpb06 <jpb.06@outlook.com>
2020-08-29 08:39:01 +0200

    Simplifying down transition

--- front-react/src/components/generic/transitions/DownTransition.tsx
+++ front-react/src/components/generic/transitions/DownTransition.tsx
@@ -1,10 +1,9 @@
 import React from "react";
+
 import Slide from "@material-ui/core/Slide";
 
-const DownTransition: React.FunctionComponent<any> = React.forwardRef(
-  (props: any, ref: any) => {
-    return <Slide direction="down" ref={ref} {...props} />;
-  }
-);
+const DownTransition: React.FC = React.forwardRef((props: any, ref: any) => (
+  <Slide direction="down" ref={ref} {...props} />
+));
 
 export default DownTransition;

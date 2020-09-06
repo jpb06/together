5934986a7d7b004d202932e50a9d847d872525ce
jpb06 <jpb.06@outlook.com>
2020-09-06 14:12:34 +0200

    Extracting mocked data

---
+++ front-react/src/test-utils/mocked-data/daily.mock.data.ts
@@ -0,0 +1,14 @@
+import { Daily } from "../../types/shared";
+
+export const dailyMockData: Daily = {
+  id: "34",
+  teamId: "23",
+  day: 1,
+  month: 1,
+  year: 2000,
+  doneTickets: [],
+  unforeseenTickets: [],
+  feelings: [],
+  subjects: [],
+  durationIndicator: "0-15",
+};
---
+++ front-react/src/test-utils/mocked-data/team.members.mock.data.ts
@@ -0,0 +1,20 @@
+export const teamMembersMockData = [
+  {
+    id: "415",
+    lastName: "Cool",
+    firstName: "Girl",
+    avatarName: "Cool.girl.gif",
+    email: "cool.girl@great.com",
+    status: "Creator",
+    joinDate: new Date().toString(),
+  },
+  {
+    id: "416",
+    lastName: "Cool",
+    firstName: "Man",
+    avatarName: "Cool.man.gif",
+    email: "cool.man@great.com",
+    status: "Member",
+    joinDate: new Date().toString(),
+  },
+];

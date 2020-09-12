01bc629e3067fc6e1fcbc8ebdde7da11f086a760
jpb06 <jpb.06@outlook.com>
2020-09-12 18:09:05 +0200

    Adding a link role to nav link

--- front-react/src/components/generic/buttons/ForwardNavLink.tsx
+++ front-react/src/components/generic/buttons/ForwardNavLink.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 // https://github.com/mui-org/material-ui/issues/15903
 const ForwardNavLink = React.forwardRef((props: any, ref: any) => (
-  <NavLink {...props} name="forward-nav-link" innerRef={ref} />
+  <NavLink {...props} role="link" name="forward-nav-link" innerRef={ref} />
 ));
 
 export default ForwardNavLink;

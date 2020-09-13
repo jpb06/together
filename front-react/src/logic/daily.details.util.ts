c55ad64e47955760f4fcf045e5c1661144fc4317
jpb06 <jpb.06@outlook.com>
2020-09-13 17:40:17 +0200

    Getting rid of the exceptions thrown (components will return null instead)

--- front-react/src/logic/daily.details.util.ts
+++ front-react/src/logic/daily.details.util.ts
@@ -1,5 +1,5 @@
-import { OverridableComponent } from "@material-ui/core/OverridableComponent";
 import { SvgIconTypeMap } from "@material-ui/core";
+import { OverridableComponent } from "@material-ui/core/OverridableComponent";
 
 export interface StaticFeedback {
   value: number;
@@ -8,22 +8,10 @@ export interface StaticFeedback {
   icon: OverridableComponent<SvgIconTypeMap>;
 }
 
-export const getStaticFeedback = (
-  list: Array<StaticFeedback>,
-  value: number
-) => {
-  const item = list.find((el) => el.value === value);
-  if (!item) throw Error(`Invalid static feedback (${value})`);
-
-  return item;
-};
+export const getStaticFeedback = (list: Array<StaticFeedback>, value: number) =>
+  list.find((el) => el.value === value);
 
 export const getStaticFeedbackIcon = (
   list: Array<StaticFeedback>,
   value: number
-) => {
-  const item = list.find((el) => el.value === value);
-  if (!item) throw Error(`Invalid static feedback (${value})`);
-
-  return item.icon;
-};
+) => list.find((el) => el.value === value);

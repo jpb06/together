5146560de79ff45ed4a5ff4f77112b946d870241
jpb06 <jpb.06@outlook.com>
2020-09-19 13:29:57 +0200

    misc: prettier

--- dev-mock-api/src/dev.api.server.ts
+++ dev-mock-api/src/dev.api.server.ts
@@ -1,29 +1,28 @@
-import * as express from "express";
-import { Express } from "express-serve-static-core";
-import * as bodyParser from "body-parser"; // pull information from HTML POST (express4)
-import * as cors from "cors";
+import bodyParser from "body-parser"; // pull information from HTML POST (express4)
+import cors from "cors";
+import express, { Express } from "express";
 
+import ErrorHandler from "./middleware/errors.handler.js";
 import extendsImplementation from "./middleware/extends.implementation.middleware.js";
+import NoRouteErrorHandler from "./middleware/no.route.error.handler.js";
+import mapCreateUserRoute from "./routes/anonymous/create.user.route.js";
 import mapLoginRoute from "./routes/anonymous/login.route.js";
+import mapAddDoneTicket from "./routes/daily/add.done.ticket.js";
+import mapAddFeeling from "./routes/daily/add.feeling.js";
+import mapAddSubject from "./routes/daily/add.subject.js";
+import mapAddUnforeseenTicket from "./routes/daily/add.unforeseen.ticket.js";
+import mapGetDaily from "./routes/daily/get.daily.js";
+import mapRemoveDoneTicket from "./routes/daily/remove.done.ticket.js";
+import mapRemoveFeeling from "./routes/daily/remove.feeling.js";
+import mapRemoveSubject from "./routes/daily/remove.subject.js";
+import mapRemoveUnforeseenTicket from "./routes/daily/remove.unforeseen.ticket.js";
+import mapSetDailyDuration from "./routes/daily/set.daily.duration.js";
+import mapCreateTeam from "./routes/team/create.team.js";
+import mapGetTeamMembers from "./routes/team/get.team.members.js";
 import mapAcceptTeamInvite from "./routes/user/accept.invite.route.js";
 import mapDeclineTeamInvite from "./routes/user/decline.invite.route.js";
 import mapGetUserTimeline from "./routes/user/get.timeline.route.js";
 import mapGetUserTeams from "./routes/user/get.user.teams.route.js";
-import mapGetTeamMembers from "./routes/team/get.team.members.js";
-import mapGetDaily from "./routes/daily/get.daily.js";
-import mapSetDailyDuration from "./routes/daily/set.daily.duration.js";
-import mapRemoveUnforeseenTicket from "./routes/daily/remove.unforeseen.ticket.js";
-import mapAddUnforeseenTicket from "./routes/daily/add.unforeseen.ticket.js";
-import mapAddDoneTicket from "./routes/daily/add.done.ticket.js";
-import mapRemoveDoneTicket from "./routes/daily/remove.done.ticket.js";
-import mapAddFeeling from "./routes/daily/add.feeling.js";
-import mapRemoveFeeling from "./routes/daily/remove.feeling.js";
-import mapAddSubject from "./routes/daily/add.subject.js";
-import mapRemoveSubject from "./routes/daily/remove.subject.js";
-import ErrorHandler from "./middleware/errors.handler.js";
-import NoRouteErrorHandler from "./middleware/no.route.error.handler.js";
-import mapCreateUserRoute from "./routes/anonymous/create.user.route.js";
-import mapCreateTeam from "./routes/team/create.team.js";
 import mapInviteUserToJoinTeam from "./routes/user/invite.user.to.join.team.route.js";
 import mapRequestToJoinTeam from "./routes/user/request.to.join.team.route.js";
 

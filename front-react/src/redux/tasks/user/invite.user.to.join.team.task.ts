8c8e6a0a751bc2fe9d3a1b4d9263a21285e415b2
jpb06 <jpb.06@outlook.com>
2020-09-22 21:13:42 +0200

    Displaying a snackbar to inform the user was properly invited

--- front-react/src/redux/tasks/user/invite.user.to.join.team.task.ts
+++ front-react/src/redux/tasks/user/invite.user.to.join.team.task.ts
@@ -4,12 +4,19 @@ import { apiCallTask } from "../";
 import { ApiRoutes } from "../../../api/api.routes.enum";
 import TogetherApi from "../../../api/setup/together.api";
 import { TerseUser } from "../../../stack-shared-code/types";
-import { ReduxActionContext as Context, ReduxActionType as Type } from "../../../types/redux";
-import { successPayloadAction } from "../../actions";
+import {
+    ReduxActionContext as Context, ReduxActionType as Type, SnackbarKind
+} from "../../../types/redux";
+import { showSnackbarAction, successPayloadAction } from "../../actions";
 
 export interface InviteUserToJoinTeamParams {
   teamId: string;
   email: string;
+}
+
+export interface InviteUserToTeamResult {
+  teamId: string;
+  user: TerseUser;
 }
 
 export function* inviteUserToJoinTeamTask(
@@ -21,7 +28,19 @@ export function* inviteUserToJoinTeamTask(
     TogetherApi.Instance.post(ApiRoutes.UserInvite, params)
   );
 
-  yield put(successPayloadAction(Type.InviteUserToTeam, context, user));
+  yield put(
+    successPayloadAction<InviteUserToTeamResult>(
+      Type.InviteUserToTeam,
+      context,
+      { teamId: params.teamId, user }
+    )
+  );
+  yield put(
+    showSnackbarAction(
+      `${user.firstName} ${user.lastName} has been invited to join your team.`,
+      SnackbarKind.Success
+    )
+  );
 
   return user;
 }

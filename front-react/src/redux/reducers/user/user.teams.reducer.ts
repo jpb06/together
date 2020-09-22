9e25575e1545c49abd0533c91da8267b6bad7bb8
jpb06 <jpb.06@outlook.com>
2020-09-22 21:13:21 +0200

    We should update the team members list if a user is invited

--- front-react/src/redux/reducers/user/user.teams.reducer.ts
+++ front-react/src/redux/reducers/user/user.teams.reducer.ts
@@ -2,6 +2,7 @@ import { TeamWithLastActivity } from "../../../stack-shared-code/types";
 import { ActionWithPayload, ReduxActionType as Type } from "../../../types/redux";
 import { isSuccessFor } from "../../identifiers/generic.actions.identifiers";
 import { initialState } from "../../store/root.state";
+import { InviteUserToTeamResult } from "../../tasks";
 
 const userTeamsReducer = (
   state: Array<TeamWithLastActivity> = initialState.userTeams,
@@ -12,6 +13,25 @@ const userTeamsReducer = (
   }
   if (isSuccessFor(Type.GetUserTeams, action.type)) {
     return action.payload as Array<TeamWithLastActivity>;
+  }
+  if (isSuccessFor(Type.InviteUserToTeam, action.type)) {
+    const payload = action.payload as InviteUserToTeamResult;
+    return state.map((team) =>
+      team.id === payload.teamId
+        ? {
+            ...team,
+            invitedUsers: [
+              ...team.invitedUsers,
+              {
+                id: "",
+                date: new Date().toISOString(),
+                invitee: payload.user,
+                referrer: payload.user,
+              },
+            ],
+          }
+        : team
+    );
   }
 
   return state;
